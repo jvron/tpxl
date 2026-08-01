1e983b0d39c3f1b91322cf49b9783ff9c59258f3
jvron <205451073+jvron@users.noreply.github.com>
2026-08-01 15:45:47 +0100

    refactor(type): replace image channels with TpxlFormat

--- libtpxl/include/tpxl/type.h
+++ libtpxl/include/tpxl/type.h
@@ -3,12 +3,21 @@
 
 #include <stdint.h>
 
+typedef enum {
+    TPXL_FORMAT_UNKNOWN = 0,
+    TPXL_FORMAT_R, 
+    TPXL_FORMAT_RG,
+    TPXL_FORMAT_RGB,
+    TPXL_FORMAT_RGBA,
+
+} TpxlFormat;
+
 typedef struct {
     int width;
     int height;
-    int channels;
+    TpxlFormat format;
     uint8_t* pixels;
-    
+
 } TpxlImage;
 
 typedef enum {
