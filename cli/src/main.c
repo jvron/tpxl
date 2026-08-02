35fe50f8c1b89f56d420f82ebf60ea3a955bc6af
jvron <205451073+jvron@users.noreply.github.com>
2026-08-02 19:47:45 +0100

    feat(cli): add help option

--- cli/src/main.c
+++ cli/src/main.c
@@ -13,16 +13,38 @@ int main(int argc, char* argv[]) {
 
     static struct option options[] = {
         {"info", no_argument, NULL, 'i'},
+        {"help", no_argument, NULL, 'h'},
         {NULL, 0, NULL, 0}
     };
 
     int opt;
-    while ((opt = getopt_long(argc, argv, "i", options, NULL)) != -1) {
+    while ((opt = getopt_long(argc, argv, "ih", options, NULL)) != -1) {
 
         switch (opt) {
             case 'i':
                 print_info = true;
                 break;
+            case 'h':
+                printf(
+                    "TPXL - Display images in the terminal\n"
+                    "\n"
+                    "Usage:\n"
+                    "    tpxl [OPTIONS] <file>\n"
+                    "\n"
+                    "Options:\n"
+                    "    -i, --info       Print image information\n"
+                    "    -h, --help       Show this help message\n"
+                    "    -V, --version    Show version information\n"
+                    "\n"
+                    "Arguments:\n"
+                    "    <file>           Image file to display\n"
+                    "\n"
+                    "Examples:\n"
+                    "    tpxl image.png\n"
+                    "    tpxl --info image.png\n"
+                    "    tpxl -h\n"
+                );
+                return EXIT_SUCCESS;
             
             default:
                 printf("Error: invalid option\n");
