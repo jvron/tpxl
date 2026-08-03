f31a016a4c6440ce2bcc5d803d766713968120a8
jvron <205451073+jvron@users.noreply.github.com>
2026-08-03 18:12:08 +0100

    feat(animation): add TpxlAnimator playback API

--- libtpxl/include/tpxl/animation.h
+++ libtpxl/include/tpxl/animation.h
@@ -2,8 +2,21 @@
 #define TPXL_ANIMATION_H
 
 #include "tpxl/type.h"
+#include <stdint.h>
+#include <stdio.h>
+
+typedef struct {
+    TpxlAnimation* animation;
+    size_t current_frame;
+    uint32_t elapsed; 
+
+} TpxlAnimator;
 
 TpxlResult tpxl_load_gif(const char* path, TpxlAnimation* animation);
+
+void tpxl_update_animation(TpxlAnimator* animator, uint32_t delta);
+TpxlImage* tpxl_get_animation_frame(TpxlAnimator* animator);
+
 void tpxl_free_animation(TpxlAnimation* animation);
 
 #endif
--- libtpxl/src/animation/animation.c
+++ libtpxl/src/animation/animation.c
@@ -1,7 +1,31 @@
+#include <stdint.h>
 #include <stdlib.h>
 
 #include "tpxl/animation.h"
 #include "tpxl/image.h"
+#include "tpxl/type.h"
+
+void tpxl_update_animation(TpxlAnimator* animator, uint32_t delta) {
+
+    animator->elapsed += delta;
+
+    uint32_t delay = animator->animation->delays[animator->current_frame];
+
+    if (animator->elapsed >= delay) {
+
+        animator->elapsed = 0;
+
+        animator->current_frame++;
+
+        if (animator->current_frame >= animator->animation->count) {
+            animator->current_frame = 0;
+        }
+    }
+}
+
+TpxlImage* tpxl_get_animation_frame(TpxlAnimator* animator) {
+    return &animator->animation->frames[animator->current_frame];
+}
 
 void tpxl_free_animation(TpxlAnimation* animation) {
 
