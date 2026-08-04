99aa3fd40ee7e52aca336b389391827b11b857a7
jvron <205451073+jvron@users.noreply.github.com>
2026-08-04 18:52:06 +0100

    feat(render): pass media type to backend

--- libtpxl/src/renderer/renderer.c
+++ libtpxl/src/renderer/renderer.c
@@ -3,11 +3,11 @@
 #include "tpxl/type.h"
 #include "tpxl/renderer.h"
 
-TpxlResult tpxl_render(TpxlContext* context, TpxlImage* image) {
+TpxlResult tpxl_render(TpxlContext* context, TpxlImage* image, TpxlMediaType media_type) {
 
     if (!context || !image) {
         return TPXL_INVALID_ARGUMENT;
     }
 
-    return tpxl_kitty_render(context, image);
+    return tpxl_kitty_render(context, image, media_type);
 }
