ef00127c616af99cd6d987951a3eea630848f4dc
jvron <205451073+jvron@users.noreply.github.com>
2026-08-04 12:40:57 +0100

    feat(viewport): add viewport initialization and fit support

---
+++ libtpxl/include/tpxl/viewport.h
@@ -0,0 +1,19 @@
+#ifndef TPXL_VIEWPORT_H
+#define TPXL_VIEWPORT_H
+
+#include <stdint.h>
+
+#include "type.h"
+
+typedef struct{
+    uint32_t x;
+    uint32_t y;
+    uint32_t width;
+    uint32_t height;
+
+} TpxlViewport;
+
+TpxlResult tpxl_init_viewport(TpxlViewport* viewport);
+TpxlResult tpxl_viewport_fit(TpxlViewport* viewport, uint32_t area_width, uint32_t area_height, uint32_t content_width, uint32_t content_height);
+
+#endif
---
+++ libtpxl/src/core/viewport.c
@@ -0,0 +1,38 @@
+#include "tpxl/viewport.h"
+#include "tpxl/type.h"
+
+TpxlResult tpxl_init_viewport(TpxlViewport* viewport) {
+    if (!viewport) {
+        return TPXL_INVALID_ARGUMENT;
+    }
+
+    viewport->width = 0;
+    viewport->height = 0;
+    viewport->x = 0;
+    viewport->y = 0;
+
+    return TPXL_OK;
+}
+
+TpxlResult tpxl_viewport_fit(TpxlViewport* viewport, uint32_t area_width, uint32_t area_height, uint32_t content_width, uint32_t content_height) {
+
+    if (!viewport || !content_width || !content_height) {
+        return TPXL_INVALID_ARGUMENT;
+    }
+
+    float scale_x = (float)area_width / content_width;
+    float scale_y = (float)area_height / content_height;
+
+    // scale to fit
+    float scale = scale_x < scale_y ? scale_x : scale_y;
+
+    // size to draw the image
+    viewport->width = content_width * scale;
+    viewport->height = content_height * scale;
+
+    // where to place the image
+    viewport->x = (area_width - viewport->width) / 2;
+    viewport->y = (area_height - viewport->height) / 2;
+
+    return TPXL_OK;
+}
