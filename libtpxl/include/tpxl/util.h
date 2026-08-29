042310ce8ea232c47871ca92304521c9830aa495
jvron <205451073+jvron@users.noreply.github.com>
2026-08-29 12:30:06 +0100

    feat(util): add microsecond sleep

--- libtpxl/include/tpxl/util.h
+++ libtpxl/include/tpxl/util.h
@@ -4,5 +4,6 @@
 #include <stdint.h>
 
 void tpxl_sleep_ms(uint32_t milliseconds);
+void tpxl_sleep_us(uint64_t microseconds);
 
 #endif
--- libtpxl/src/util/sleep.c
+++ libtpxl/src/util/sleep.c
@@ -6,8 +6,14 @@
 
 #include <windows.h>
 
-void tpxl_sleep_ms(uint32_t milliseconds)
-{
+void tpxl_sleep_ms(uint32_t milliseconds) {
+
+    Sleep(milliseconds);
+}
+
+void tpxl_sleep_us(uint64_t microseconds) {
+    
+    DWORD milliseconds = (DWORD)((microseconds + 999) / 1000);
     Sleep(milliseconds);
 }
 
@@ -15,11 +21,21 @@ void tpxl_sleep_ms(uint32_t milliseconds)
 
 #include <time.h>
 
-void tpxl_sleep_ms(uint32_t milliseconds)
-{
+void tpxl_sleep_ms(uint32_t milliseconds) {
+
     struct timespec ts = {
         .tv_sec = milliseconds / 1000,
         .tv_nsec = (long)(milliseconds % 1000) * 1000000L
+    };
+
+    nanosleep(&ts, NULL);
+}
+
+void tpxl_sleep_us(uint64_t microseconds) {
+    
+    struct timespec ts = {
+        .tv_sec = microseconds / 1000000,
+        .tv_nsec = (long)(microseconds % 1000000) * 1000L
     };
 
     nanosleep(&ts, NULL);
