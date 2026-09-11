95fe310fc9cec814a3e666cf5a5d38771212e7de
jvron <205451073+jvron@users.noreply.github.com>
2026-09-12 00:05:08 +0100

    refactor(cli): update video progress display

--- cli/play_video.c
+++ cli/play_video.c
@@ -9,50 +9,24 @@
 
 #include "cli.h"
 
-static void print_video_progress(double current, double duration) {
-
-    const int bar_width = 40;
-
-    double progress = 0.0;
-
-    if (duration > 0.0) {
-        progress = current / duration;
-    }
-
-    if (progress < 0.0)
-        progress = 0.0;
-
-    if (progress > 1.0)
-        progress = 1.0;
-
-    int position = (int)(progress * bar_width);
+static void print_video_bar(TpxlVideoPlayer* player, double fps, double current, double duration) {
 
     int current_sec = (int)current;
     int duration_sec = (int)duration;
 
     printf(
-        "\r%02d:%02d [",
+        "\r%02d:%02d/",
         current_sec / 60,
         current_sec % 60
     );
 
-    for (int i = 0; i < bar_width; i++) {
-        if (i < position) {
-            putchar('=');
-        } 
-        else if (i == position) {
-            putchar('+');
-        } 
-        else {
-            putchar('-');
-        }
-    }
-
     printf(
-        "] %02d:%02d",
+        "%02d:%02d ",
         duration_sec / 60,
         duration_sec % 60
     );
+
+    printf("frame=%u fps=%.2f", tpxl_get_frames_played(player), fps);
 }
 
 int play_video(const char* path, TpxlContext* context) {
@@ -102,19 +76,41 @@ int play_video(const char* path, TpxlContext* context) {
     uint32_t video_rows = (context->viewport.height + context->terminal.cell_height - 1) / context->terminal.cell_height;
     uint32_t video_cols = context->viewport.x / context->terminal.cell_width;
 
-    TpxlRenderer* renderer = NULL;
-
     TpxlFormat format;
     tpxl_get_video_format(video, &format);
 
     uint32_t output_width, output_height;
     result = tpxl_get_video_output_dimensions(video, &output_width, &output_height);
 
+    TpxlRenderer* renderer = NULL;
     result = tpxl_create_renderer(&renderer, context, output_width, output_height, format, TPXL_MEDIA_ANIMATED);
 
     if (result != TPXL_OK) {
         printf("Error: %s\n", tpxl_result_to_string(result));
         tpxl_close_video(&video);
+        return EXIT_FAILURE;
+    }
+
+    TpxlImage background = {0};
+    background.width = output_width;
+    background.height = output_height;
+    background.format = TPXL_FORMAT_RGB;
+    background.pixels = calloc(1, output_width * output_height * tpxl_format_to_channels(TPXL_FORMAT_RGB));
+
+    if (!background.pixels) {
+        printf("Error: %s\n", tpxl_result_to_string(result));
+        tpxl_close_video(&video);
+        tpxl_destroy_renderer(&renderer);
+        return EXIT_FAILURE;
+    }
+
+    result = tpxl_renderer_render(renderer, &background);
+    free(background.pixels);
+
+    if (result != TPXL_OK) {
+        printf("Error: %s\n", tpxl_result_to_string(result));
+        tpxl_close_video(&video);
+        tpxl_destroy_renderer(&renderer);
         return EXIT_FAILURE;
     }
 
@@ -139,23 +135,22 @@ int play_video(const char* path, TpxlContext* context) {
         return EXIT_FAILURE;
     }
 
+    double fps = tpxl_get_video_frame_rate(video);
+
     double duration = tpxl_get_video_duration(video);
 
     while (tpxl_video_playing(player)) {
         double current = tpxl_get_video_time(player);
 
         printf("\033[%uB", video_rows + 1);
-        print_video_progress(current, duration);
+        print_video_bar(player, fps, current, duration);
         printf("\033[%uA\033[%uG", video_rows + 1, video_cols);
         fflush(stdout);
 
-        tpxl_sleep_ms(100);
+        tpxl_sleep_ms(500);
     }
 
-    printf("\n");
-    printf("Frame count: %d\n", tpxl_get_video_frame_count(video));
-    printf("\033[%uB", video_rows + 1);
-    fflush(stdout);
+    printf("\033[%uB", video_rows + 2);
 
     tpxl_close_video_player(&player);
     tpxl_destroy_renderer(&renderer);
